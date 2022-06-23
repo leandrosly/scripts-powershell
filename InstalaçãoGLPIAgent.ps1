be1d72e81d6ef6016391a3325c9cd9f0015ac738
Leandro Serra Acosta <2184290+leandrosly@users.noreply.github.com>
2022-06-23 07:27:24 -0300

    Commit inicial
    Scripts funcionais

---
+++ InstalaçãoGLPIAgent.ps1
@@ -0,0 +1,55 @@
+﻿# Instalação do GLPI Inventory Agent via GPO
+
+# Repositorio oficial
+# https://github.com/glpi-project/glpi-agent/releases
+
+# Arquivo de instalação 64 bits:
+$msi64 = "https://github.com/glpi-project/glpi-agent/releases/download/1.3/GLPI-Agent-1.3-x64.msi"
+
+# Arquivo de instalação 32 bits:
+$msi32 = "https://github.com/glpi-project/glpi-agent/releases/download/1.3/GLPI-Agent-1.3-x86.msi"
+
+# Versão do instalador atualizado:
+$versao = "1.3"
+$match = 'This is GLPI Agent ' + $versao
+
+Function Instalar {
+
+  if ([Environment]::Is64BitProcess) {
+
+    msiexec /i $msi64 SERVER=glpi.ipassp.intra /qn
+
+  } else {
+
+    msiexec /i $msi32 SERVER=glpi.ipassp.intra /qn
+
+  }
+
+}
+
+# Verificar se o fusion esta instalado e remover
+if ((Get-Service -Name "*FusionInventory*" -ErrorAction SilentlyContinue) -ne $null) {
+
+  Stop-Service 'FusionInventory-Agent'; Get-CimInstance -ClassName Win32_Service -Filter "Name='FusionInventory-Agent'" | Remove-CimInstance
+
+}
+
+# Verifica se o agente glpi ja esta instalado
+if ((Get-Service -Name "*glpi-agent*" -ErrorAction SilentlyContinue) -ne $null) {
+
+  # Se a versao instalada nao for a atual, instalar a nova
+  if ((Invoke-WebRequest -Uri "http://localhost:62354" -UseBasicParsing).Content -notmatch $match) {
+
+    Instalar
+
+  } else {
+
+    echo "ja instalado"
+
+  }
+
+} else {
+
+  Instalar
+
+}
---
+++ InstalaçãoZabbixAgent2.ps1
@@ -0,0 +1,50 @@
+﻿# Instalação do Zabbix Agent 2 via GPO
+
+# Repositorio oficial
+# https://cdn.zabbix.com/zabbix/binaries/stable/6.0/
+
+# Arquivo de instalação 64 bits:
+$msi64="https://cdn.zabbix.com/zabbix/binaries/stable/6.0/6.0.5/zabbix_agent2-6.0.5-windows-amd64-openssl.msi"
+
+# Arquivo de instalação 32 bits:
+$msi32="https://cdn.zabbix.com/zabbix/binaries/stable/6.0/6.0.5/zabbix_agent2-6.0.5-windows-i386-openssl.msi"
+
+# Versão do instalador atualizado:
+$versao="6.0.5"
+
+# Endereço do servidor:
+$servidor = "zabbix.ipassp.intra"
+
+################################################
+
+Function Instalar {
+
+  $nome = ([Environment]::MachineName)
+
+  if ([Environment]::Is64BitProcess) {
+
+    msiexec /i $msi64 HOSTNAME=$nome SERVER=$servidor /qn
+
+  } else {
+
+    msiexec /i $msi32 HOSTNAME=$nome SERVER=$servidor /qn
+
+  }
+
+}
+
+# Verifica se o agente ja esta instalado
+if ((Get-Service -Name "zabbix agent*" -ErrorAction SilentlyContinue) -ne $null) {
+
+  # Verifica a versao instalada
+  if ((& 'C:\Program Files\Zabbix Agent 2\zabbix_agent2.exe' -V).Split([Environment]::NewLine)[0] -replace '^.* (\d+(\.\d+){2})$', '$1' -ne $versao) {
+
+    Instalar
+
+  }
+
+} else {
+
+  Instalar
+
+}
