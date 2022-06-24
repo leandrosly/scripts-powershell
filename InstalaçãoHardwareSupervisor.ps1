b045244d83a929642f43ee72f537defa7f90159d
Leandro Serra Acosta <leandro.acosta@ipassp.intra>
2022-06-24 12:01:28 -0300

    Novo script para instalar o serviço Hardware Supervisor para monitorar as temperaturas

---
+++ InstalaçãoHardwareSupervisor.ps1
@@ -0,0 +1,29 @@
+﻿# Instalação do Hardware Supervisor via GPO
+
+# Repositorio oficial
+# https://github.com/darkbrain-fc/HardwareSupervisor/releases/
+
+# Arquivo de instalação 64 bits:
+$msi = "https://github.com/darkbrain-fc/HardwareSupervisor/releases/download/0.3.1/HardwareSupervisorSetup.msi"
+
+# Versão do instalador atualizado:
+#$versao = "0.3.1"
+
+####################################################
+
+Function Instalar {
+
+    msiexec /i $msi /qn
+
+}
+
+# Verifica se esta instalado
+if ((Get-Service -Name "*hardwaresupervisor*" -ErrorAction SilentlyContinue) -ne $null) {
+
+  echo "ja instalado"
+
+} else {
+
+  Instalar
+
+}
